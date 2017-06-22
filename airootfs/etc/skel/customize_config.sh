96a6ac78b55ac45d793066b0f04e0677fec85702
Tristan Claverie <public@tclaverie.eu>
2017-06-22 08:49:14 +0200

    Add user config

---
+++ airootfs/etc/skel/customize_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.background picture-uri file://$HOME/.config/background.png
+
+gsettings set org.gnome.desktop.input-sources sources \"[('xkb', 'fr')]\"
++
--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -8,18 +8,16 @@ echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
 echo "LANGUAGE=fr" > /etc/locale.conf
 echo "LC_ALL=fr_FR.UTF-8" > /etc/locale.conf
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 
 useradd $USER --create-home -U
 echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
 echo "KEYMAP=fr" > /etc/vconsole.conf
 
-# This one is tricky. Changing GNOME settings reguire using gsettings, which uses dconf, and it needs dbus lauched. Yet, to run `gsettings` commands successfully, a dbus daemon needs to be started, but we are in a chroot. This is a workaround to start dbus in this environment, to launch a set of gsettings commands.
-su $USER -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.input-sources sources \"[('xkb', 'fr')]\""
-
 # Do not create default directories, we do not want them
 sed -i 's/\(enabled=\)True/\1False/' /etc/xdg/user-dirs.conf
 
+# Autologin of GDM
 echo "# GDM configuration storage
 
 [daemon]
@@ -35,14 +33,18 @@ AutomaticLoginEnable=True
 [debug]
 " > /etc/gdm/custom.conf
 
+# Allow no password login
 sed -i '1s/^/auth sufficient pam_succeed_if.so user ingroup nopasswdlogin\n/' /etc/pam.d/gdm-password
 groupadd nopasswdlogin
 usermod -a -G nopasswdlogin $USER
 
 
-usermod -s /usr/bin/zsh root
-cp -aT /etc/skel/ /root/
-chmod 700 /root
+usermod -s /usr/bin/zsh $USER
+cp -aT /etc/skel/. /home/$USER
+chown $USER:$USER /home/$USER -R
+
+# This one is tricky. Changing GNOME settings reguire using gsettings, which uses dconf, and it needs dbus lauched. Yet, to run `gsettings` commands successfully, a dbus daemon needs to be started, but we are in a chroot. This is a workaround to start dbus in this environment, to launch a set of gsettings commands.
+su $USER -c "dbus-launch --exit-with-session ~/customize_config.sh"
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
